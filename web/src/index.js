8fe0b98831aff428f3124860a5ce8c53e83140f2
Jie Meng <jmengxy@gmail.com>
2018-08-21 11:48:45 +0800

    Remove useless code

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,3 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-//import './index.css';
-//import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
-//
-//ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
-
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom';
