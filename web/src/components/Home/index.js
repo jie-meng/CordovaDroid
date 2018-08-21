1daad8d95f75043ad9b60e6272ae89115c14fe03
Jie Meng <jmengxy@gmail.com>
2018-08-21 15:36:54 +0800

    Support scss

--- web/src/components/Home/index.js
+++ web/src/components/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Home.scss'
+
+const cx = classNames.bind(styles);
 
 class Home extends React.Component {
   render() {
     return (
-        <div>
+        <div className={ cx('home') }>
             Home
         </div>
     );
